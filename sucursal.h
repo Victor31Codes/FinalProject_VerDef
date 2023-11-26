7f701493686f6f3f61451a6fa52e761f8867ddba
PAnd0509 <paola2010andrea05@gmail.com>
2023-11-26 18:38:02 -0500

    Sucursal con lista de empleados

--- sucursal.h
+++ sucursal.h
@@ -2,13 +2,15 @@
 #define SUCURSAL_H
 
 #include "string"
-
+#include "lista.h"
+#include "empleado.h"
 using namespace std;
 
 class Sucursal {
 	public:
 	int id_ciudad, id_sucursal;
-    string nombre_sucursal, dir_sucursal, barrio_sucursal, nombre_gerente; 
+    string nombre_sucursal, dir_sucursal, barrio_sucursal, nombre_gerente;
+    Lista<Empleado> empleados;
 };
 
 #endif
